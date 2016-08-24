e381475aa748d065fd013037178d831b64b200bd
TaXxX <henrry_el_master@hotmail.com>
2016-08-24 18:56:14 -0400

    fixed functionality datePicker

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,9 +6,38 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController() {
+  function MainController(LxDatePickerService) {
     var vm = this;
+    vm.locale = 'en';
 
+    vm.datePicker = {
+      basic:
+      {
+        date: new Date(),
+        dateFormatted: moment().locale(vm.locale).format('LL'),
+        minDate: new Date(new Date().getFullYear(), new Date().getMonth() - 2, new Date().getDate()),
+        maxDate: new Date(new Date().getFullYear(), new Date().getMonth() + 2, new Date().getDate())
+      },
+      input:
+      {
+        date: new Date(),
+        dateFormatted: moment().locale(vm.locale).format('LL')
+      }
+    };
+    vm.datePickerId = 'date-picker';
+
+    ///////////
+
+    vm.datePickerCallback = function(_newdate)
+    {
+      vm.datePicker.basic.date = _newdate;
+      vm.datePicker.basic.dateFormatted = moment(_newdate).locale(vm.locale).format('LL');
+    };
+
+    vm.openDatePicker = function(_pickerId)
+    {
+      LxDatePickerService.open(_pickerId);
+    };
 
     vm.user = [
       {
